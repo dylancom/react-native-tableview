0ade2eb8a29174dc9d58bf8fab661a867f7eb0bd
Dylan <dylancompanjen@gmail.com>
2020-06-10 17:47:26 +0200

    Create CustomCellSectionIndex.m

---
+++ RNTableView/CustomCellSectionIndex.m
@@ -0,0 +1,37 @@
+#import "CustomCellSectionIndex.h"
+
+#define MARGIN 12
+
+@implementation CustomCellSectionIndex
+
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    self.imageView.frame = CGRectMake(16, 8, 48, 48);
+    
+    CGRect cvf = self.contentView.frame;
+    CGRect frame = CGRectMake(cvf.size.height + MARGIN,
+                              self.textLabel.frame.origin.y,
+                              cvf.size.width - cvf.size.height - 2*MARGIN,
+                              self.textLabel.frame.size.height);
+    self.textLabel.frame = frame;
+
+    frame = CGRectMake(cvf.size.height + MARGIN,
+                       self.detailTextLabel.frame.origin.y,
+                       cvf.size.width - cvf.size.height - 2*MARGIN,
+                       self.detailTextLabel.frame.size.height);
+    self.detailTextLabel.frame = frame;
+    
+    CGRect adjustedFrame = self.accessoryView.frame;
+    adjustedFrame.origin.x = self.frame.size.width - 48 - 20;
+    self.accessoryView.frame = adjustedFrame;
+}
+
+@end
